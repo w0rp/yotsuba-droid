ca7a68f19357fa6cb7597615dd175dd05148f09a
w0rp <moebiuspersona@gmail.com>
2013-02-09 12:04:11 +0000

    Applied Linkify to comments.

--- YotsubaDroid/src/com/w0rp/yotsubadroid/ThreadViewAdapter.java
+++ YotsubaDroid/src/com/w0rp/yotsubadroid/ThreadViewAdapter.java
@@ -1,5 +1,6 @@
 package com.w0rp.yotsubadroid;
 
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,22 +12,23 @@ public class ThreadViewAdapter extends PostListAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View item = convertView;
-        
+
         if (item == null) {
             LayoutInflater inf = LayoutInflater.from(parent.getContext());
-            
+
             item = (RelativeLayout) inf.inflate(
                 R.layout.thread_post, parent, false);
         }
-        
+
         final Post post = (Post) getItem(position);
-        
+
         ImageView imageView = (ImageView) item.findViewById(R.id.post_image);
         loadImage(post, imageView);
-        
+
         TextView comment = (TextView) item.findViewById(R.id.post_comment);
         comment.setText(Comment.fullText(post.getComment()));
-        
+        Linkify.addLinks(comment, Linkify.WEB_URLS);
+
         return item;
     }
 
